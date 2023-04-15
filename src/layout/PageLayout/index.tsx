f81d4d945188928a9542ed53546b9556a767613c
Approximations <datkidbosst@gmail.com>
2023-04-15 01:00:44 -0300

    pagelayout

--- src/layout/PageLayout/index.tsx
+++ src/layout/PageLayout/index.tsx
@@ -3,15 +3,15 @@ import Header from '@/layout/Header';
 import Footer from '@/layout/Footer';
 
 type Props = {
-    children: React.ReactNode
-}
+    children: React.ReactNode;
+};
 
-export default function PageLayout({children}: Props) {
+export default function PageLayout({ children }: Props) {
     return (
         <>
-            <Header/>
-            <main>{children}</main>
-            <Footer/>
+            <Header />
+            {children}
+            <Footer />
         </>
     );
 }
